ece737402912de6e3c228323c404f6cf3a369a76
kimmohakkarainen <30873934+kimmohakkarainen@users.noreply.github.com>
2021-04-10 10:22:20 +0300

    code changes

--- src/App.js
+++ src/App.js
@@ -8,32 +8,21 @@ import GraphView from "./graph";
 
 const data1 = {
   nodes: [
-    { index: 0, name: "eka", value: 60, hours: "1:00" },
-    { index: 1, name: "toka", value: 120, hours: "2:00" },
-    { index: 2, name: "kol", value: 180, hours: "3:00" },
-    { index: 3, name: "nel", value: 80, hours: "1:20" },
-    { index: 4, name: "vii", value: 100, hours: "1:40" }
+    { index: 0, name: "Liikevaihto", value: 100, hours: "100%" },
+    { index: 1, name: "Kiinteät kulut", value: 75, hours: "85%" },
+    { index: 2, name: "Muuttuvat kulut", value: 10, hours: "3:00" },
+    { index: 3, name: "Palkkakulut", value: 69, hours: "1:20" },
+    { index: 4, name: "Muut kiinte", value: 6, hours: "1:40" },
+    { index: 5, name: "Kate", value: 15, hours: "1:40" }
   ],
   links: [
-    { source: 0, target: 2, value: 60, hours: "+1:00" },
-    { source: 1, target: 2, value: 120, hours: "+2:00" },
-    { source: 2, target: 3, value: 80, hours: "+1:20" },
-    { source: 2, target: 4, value: 100, hours: "+1:40" }
+    { source: 0, target: 1, value: 75, hours: "+1:00" },
+    { source: 0, target: 2, value: 10, hours: "+2:00" },
+    { source: 1, target: 3, value: 69, hours: "+1:20" },
+    { source: 1, target: 4, value: 6, hours: "+1:40" },
+    { source: 0, target: 5, value: 15, hours: "+1:40" }
   ]
 };
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 export default function App() {
   return (
